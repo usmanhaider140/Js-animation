94f850101af32275feae882ea1054e14c516390f
usmanhaider140 <usmanhaider461@outlook.com>
2022-08-15 19:02:32 +0500

    - add logo text animation

--- text-anime/app.js
+++ text-anime/app.js
@@ -93,3 +93,25 @@ timeline.fromTo(
   },
   "<20%"
 );
+
+// Split Text Animation
+
+const logo = document.querySelector(".logo");
+
+const letters = logo.textContent.split("");
+logo.textContent = "";
+
+letters.forEach((letter, index) => {
+  const span = document.createElement("span");
+  span.textContent = letter;
+  span.classList.add("letter");
+  logo.appendChild(span);
+});
+
+gsap.set(".letter", { display: "inline-block" });
+
+gsap.fromTo(
+  ".letter",
+  { y: "100%", opacity: 0 },
+  { y: 0, delay: 1, opacity: 1, stagger: 0.05, ease: "back.out(3)" }
+);
